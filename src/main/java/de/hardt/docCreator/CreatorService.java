68b2102bc5318ed66d9ebbc6123ab51f9b6b23e2
jenshardt@gmx.net <derSatan>
2017-06-10 22:19:59 +0200

    implemented pdf controller

---
+++ src/main/java/de/hardt/docCreator/CreatorService.java
@@ -0,0 +1,65 @@
+package de.hardt.docCreator;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.fop.apps.FOPException;
+import org.apache.fop.apps.FOUserAgent;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.MimeConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreatorService {
+	
+	@Autowired
+	private Config config;
+
+	public String convertToPDF() throws IOException, FOPException, TransformerException {
+		String response = "";
+		// the XSL FO file
+		File xsltFile = new File(config.getRootPath() + "\\files\\templates\\simple.xsl");
+		// the XML file which provides the input
+		StreamSource xmlSource = new StreamSource(new File(config.getRootPath() + "\\files\\input\\simple.xml"));
+		// create an instance of fop factory
+		FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
+		// a user agent is needed for transformation
+		FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+		// Setup output
+		OutputStream out;
+		out = new java.io.FileOutputStream(config.getRootPath() + "\\files\\output\\simple.pdf");
+
+		try {
+			// Construct fop with desired output format
+			Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+
+			// Setup XSLT
+			TransformerFactory factory = TransformerFactory.newInstance();
+			Transformer transformer = factory.newTransformer(new StreamSource(xsltFile));
+
+			// Resulting SAX events (the generated FO) must be piped through to
+			// FOP
+			Result res = new SAXResult(fop.getDefaultHandler());
+
+			// Start XSLT transformation and FOP processing
+			// That's where the XML is first transformed to XSL-FO and then
+			// PDF is created
+			transformer.transform(xmlSource, res);
+		} finally {
+			out.close();
+			response = "Generated PDF";
+		}
+		
+		return response;
+	}
+}
--- src/main/java/de/hardt/docCreator/controller/CreatorController.java
+++ src/main/java/de/hardt/docCreator/controller/CreatorController.java
@@ -1,15 +1,41 @@
 package de.hardt.docCreator.controller;
 
+import java.io.IOException;
+
+import javax.xml.transform.TransformerException;
+
+import org.apache.fop.apps.FOPException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import de.hardt.docCreator.CreatorService;
 
 @RestController
 public class CreatorController {
 
     private static final String template = "Hello, this is a simple REST service creating a pdf file with FOP";
+    
+    @Autowired
+    private CreatorService cs;
 
     @RequestMapping("/creator/info")
     public String info() {
         return String.format(template);
+    }
+    
+    @RequestMapping("/creator/pdf")
+    public String pdf() {
+    	String response = "";
+    	
+    	try {
+			response = cs.convertToPDF();
+		} catch (FOPException | IOException | TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			response = "Something went wrong!";
+		}
+    	
+        return response;
     }
 }
