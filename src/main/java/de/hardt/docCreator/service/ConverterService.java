b1d442c9397a472a9064bf291d3af6e2f0f7bdcb
jenshardt@gmx.net <derSatan>
2017-06-12 21:36:24 +0200

    implement pdf2html convert

--- src/main/java/de/hardt/docCreator/service/ConverterService.java
+++ src/main/java/de/hardt/docCreator/service/ConverterService.java
@@ -1,8 +1,16 @@
 package de.hardt.docCreator.service;
 
 import java.io.IOException;
+import java.io.StringWriter;
 
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.fit.pdfdom.PDFDomTree;
@@ -18,10 +26,10 @@ public class ConverterService {
 	@Autowired
 	private Config config;
 
-	public String convertPDFToHTML() throws IOException, ParserConfigurationException {
+	public String convertPDFToHTML(String inputFileName, String outputFileName) throws IOException, ParserConfigurationException, TransformerException {
 		String response = "";
 		// load the PDF file using PDFBox 
-		PDDocument pdf = PDDocument.load(new java.io.File("file.pdf")); 
+		PDDocument pdf = PDDocument.load(new java.io.File(config.getRootPath() + "//input//" + inputFileName)); 
 		
 		// create the DOM parser 
 		PDFDomTree parser = new PDFDomTree(); 
@@ -29,6 +37,16 @@ public class ConverterService {
 		// parse the file and get the DOM Document 
 		Document dom = parser.createDOM(pdf);
 		
+		// Get HTML via javax.transformer
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer trans = tf.newTransformer();
+        trans.setOutputProperty(OutputKeys.INDENT, "yes");
+        trans.setOutputProperty(OutputKeys.ENCODING, "utf-8");
+        trans.setOutputProperty(OutputKeys.METHOD, "html");
+        StringWriter sw = new StringWriter();
+        trans.transform(new DOMSource(dom), new StreamResult(sw));
+        String html = sw.toString();
+	      
 		return response;
 	}
 }
